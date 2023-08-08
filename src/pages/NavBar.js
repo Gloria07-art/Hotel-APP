12ffba9692dcd4bc4c411627f6f7e5fdb2d01e38
Gloria Pucoe <79689244+Gloria07-art@users.noreply.github.com>
2023-08-08 15:10:09 +0200

    Update NavBar.js

--- src/pages/NavBar.js
+++ src/pages/NavBar.js
@@ -1,18 +1,33 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import Img from "./assets/img.png";
+import "./styles/NavBar.css";
 
 const NavBar = () => {
   return (
-    <div>
-        <div className="nav-bar"> </div>
-      <nav>
-        <div> <ul></ul>
-        <NavLink to="/"> Contact Us</NavLink><ul></ul>
-        <NavLink to="login"> Reviews</NavLink><ul></ul>
-        <NavLink to="login"> Rooms</NavLink><ul></ul>
+    <div style={{display:'flex' , width:"100%", justifyContent:'space-between', margin:'0'}}>
+      <div className="nav-bar">
+        <div className="Logo">
+        <img
+          src={Img}
+          alt=""
+          style={{
+            
+            marginRight: "90%",
+            marginBottom: "1%",
+             marginTop: "2px",
+            width: "180px",
+            height: "70px",
+          }}
+        />
         </div>
-      </nav>
+      </div>
+
+      <div style={{width:'50%'}} >
+        <NavLink to="/" className="navLink"> Contact Us</NavLink>
+        <NavLink to="login" className="navLink" > Reviews</NavLink>
+        <NavLink to="login" className="navLink"> Rooms</NavLink>
+      </div>
     </div>
   );
 };
