042ff8987029e03d19a4e25f2b369cf1a9772d5b
Gloria Pucoe <79689244+Gloria07-art@users.noreply.github.com>
2023-08-08 09:10:05 +0200

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,42 +1,79 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import NavBar from "./NavBar";
-
+import Room1 from "./assets/Room1.jpg";
+import Room2 from "./assets/Room2.jpg";
+import Room3 from "./assets/Room3.jpg";
+import Room4 from "./assets/Room4.jpg";
+import Room5 from "./assets/Room5.jpg";
+import Room6 from "./assets/Room6.jpg";
+import './styles/home.css'
 
 export const Home = () => {
+  return (
+    <div>
+      <NavBar />
 
-const images= [
-    {id:1, src: './assets/Room1', alt: 'Room1'},
-    {id:2, src: './assets/Room2', alt: 'Room2'},
-    {id:3, src: './assets/Room3', alt: 'Room3'},
-    {id:4, src: './assets/Room4', alt: 'Room4'}
-]
-return (
+      <div>
 
-    
+        <div className="room-container" >
+        
+          <img src={Room1} alt="" style={{ width: "10%" }} />
+          <div>
+          <h1>Room 1</h1>
+          </div>
+          
+          <p>
+            This room exudes the summery vibes of summery <br></br>summer and
+            you will feel refeshed after a long sleep.
+          </p>
+        
+        </div>
+
+        <div> 
+          
+          <img src={Room2} alt="" style={{ width: "10%" }} />
+           <div> <h1>Room 2</h1></div>
+          <div> <p>
+            This room exudes the summery vibes of summery <br></br>summer and
+            you will feel refeshed after a long sleep.
+          </p></div>
+         
+        
+        </div>
+
+        <div>
+       
+          <img src={Room3} alt="" style={{ width: "10%" }} />
+           <div><h1>Room 3</h1></div>
+          <div><p>
+            This room exudes the summery vibes of summery <br></br>summer and
+            you will feel refeshed after a long sleep.
+          </p></div>
+           
+           </div>
+
+          <div>
+     
+          <img src={Room4} alt="" style={{ width: "10%" }} />
+          <div>  <h1>Room 4</h1></div>
+         <div> <p>
+            This room exudes the summery vibes of summery <br></br>summer and
+            you will feel refeshed after a long sleep.
+          </p></div> 
+         
+        </div>
 <div>
-<NavBar />
+        <input type="date" placeholder="enter email" />
+        
+        <br></br> <br></br>
+        <input type="date" placeholder="enter password" />
+        </div>
+        <br></br> <br></br>
+      </div>
+      <div></div>
+      <button>logout</button>
+    </div>
+  );
 
-<div>
-<input type="date" 
-placeholder="enter email"
-/> 
-{""}
- <br></br> <br></br>
 
-<input type="date"  
- placeholder="enter password"
- /> 
- {""}
- <br></br> <br></br>
- </div>
- <div>
-
- </div>
-<button >logout</button>
-
-</div>
-
-);
-    
 };
-
